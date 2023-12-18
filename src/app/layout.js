ded6c6d7b1484f356c73a022406f21a574bcbe6b
Muhamad Ridho Ardian <49930863+pockypaw@users.noreply.github.com>
2023-12-18 14:11:03 +0700

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,10 +1,11 @@
 'use client'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import type { Metadata } from 'next'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata = {
+export const metadata: Metadata = {
   title: 'dicoding:email',
   openGraph: {
     title: 'ardridhz@gmail.com',
