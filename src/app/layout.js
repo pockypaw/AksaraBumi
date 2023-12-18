e038dcf1cf0f6d61f82ace5ce0f49be9d2e5420d
Muhamad Ridho Ardian <49930863+pockypaw@users.noreply.github.com>
2023-12-18 14:01:30 +0700

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,6 +12,9 @@ const inter = Inter({ subsets: ['latin'] })
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+     <head>
+        <meta name="dicoding:email" content="ardridhz@gmail.com">
+     </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
